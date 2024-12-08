0aa9a0b720aa643c13586a87830cde51b9146e1d
Alexey Potory <alexey.potory@gmail.com>
2024-12-08 13:54:23 +0500

    Changed directories for games

--- src/core/games/simple-dice-game.ts
+++ src/core/games/simple-dice-game.ts
@@ -1,8 +1,8 @@
 import TurnBasedGame from "./turn-based-game";
 
-export class AlZahrGame extends TurnBasedGame {
+export class SimpleDiceGame extends TurnBasedGame {
     async _onNextTurn(): Promise<void> {
         await super._onNextTurn();
-        console.log('Al Zahr Game');
+        console.log('Simple Dice Game');
     }
 }
